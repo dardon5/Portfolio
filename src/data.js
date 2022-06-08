e485c9856f1f48dfb7c8a98c044ea01847904727
dardon5 <alejandro_dardon@hotmail.com>
2022-06-08 14:05:11 -0600

    Small changes

--- src/data.js
+++ src/data.js
@@ -3,8 +3,6 @@ export const featuredPortfolio = [
     id: 1,
     title: "Social Media App",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
   },
   {
     id: 2,
